b7c80e7cfc277fe5838af21a5d93596af67a1a68
Smulimaan <samuli.tyyska@live.com>
2022-03-13 19:27:34 +0200

    project1 first

---
+++ Projects/Guestbook/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello Sam");
+});
+
+app.get("*", function (req, res) {
+  res.send("Can't find the requested page", 404);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
