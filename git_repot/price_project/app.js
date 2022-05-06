922ea727bb3a9c28adc36503f7cd5187e8daed16
Smulimaan <samuli.tyyska@live.com>
2022-05-06 18:33:23 +0300

    update

--- git_repot/price_project/app.js
+++ git_repot/price_project/app.js
@@ -1,65 +1,66 @@
 var express = require("express");
+const m3o = require("@m3o/m3o-node");
+const { PriceService } = require("m3o/price");
 var app = express();
 const port = process.env.PORT || 3000;
+M3O_API_TOKEN = "NGMyZWYyMzAtMzRmMy00MjkyLTk2YmQtNGQzOWM1YzNiYjAx";
 
 app.set("view engine", "ejs");
-//app.locals.pretty = true;
 
 //luodaan reitit
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
-app.get("/hello", function (req, res) {
-  const m3o = require("@m3o/m3o-node");
 
-  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
-    .call("helloworld", "call", { name: "Samuli" })
-    .then((response) => {
-      console.log(response);
-      res.send("Hello World!" + JSON.stringify(response));
-    });
-});
 app.get("/food", function (req, res) {
-  const m3o = require("@m3o/m3o-node");
-  var list = "";
-
-  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
+  //lisätään list muuttuja
+  let list = "";
+  //luodaan m3o luokasta uusi olio
+  new m3o.Client({ token: M3O_API_TOKEN })
+    //api kutsu
     .call("price", "list", { currency: "EUR", limit: 200, offset: 0 })
+    //käsitellään api vastaus
     .then((response) => {
+      // kopioidaan api vastaus list muuttujaan
       list = response;
-
+      // renderöidään list muutujan tiedon food pagelle
       res.render("pages/food", list);
     });
-  //console.log(hinta);
 });
 app.get("/oil", function (req, res) {
-  const m3o = require("@m3o/m3o-node");
   var hinta = "";
-
-  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
+  new m3o.Client({ token: M3O_API_TOKEN })
     .call("price", "get", { name: "WTI crude oil", currency: "EUR" })
     .then((response) => {
       hinta = response;
-
-      //res.send(JSON.stringify(hinta));
       res.render("pages/oil", hinta);
     });
-  //console.log(hinta);
 });
 app.get("/crypto", function (req, res) {
-  const m3o = require("@m3o/m3o-node");
   var list = "";
-
-  new m3o.Client({ token: "MTk3ZTNkZmEtNjExOS00MjBkLThkNDktNWI2OGE0YTVkYTZh" })
+  new m3o.Client({ token: M3O_API_TOKEN })
     .call("price", "list", { currency: "EUR", limit: 200, offset: 0 })
     .then((response) => {
       list = response;
-      console.log(list);
-
-      //res.send(JSON.stringify(hinta));
       res.render("pages/crypto", list);
     });
 });
+
+//tässä on testi reitti funktion sisälle tehtyyn api kutsuun. Näin tehtynä olisi helpompi paketoida koodia funktioon reitin ulkopuolelle jatkossa
+app.get("/test", function (req, res) {
+  const priceService = new PriceService(M3O_API_TOKEN);
+  rsp = "";
+  // Get the price of anything
+  async function getThePrice() {
+    const rsp = await priceService.get({
+      currency: "USD",
+      name: "bitcoin",
+    });
+    res.send(rsp);
+  }
+  getThePrice();
+});
+
 app.get("*", function (req, res) {
   res.send("Can't find the requested page!", 404);
 });
